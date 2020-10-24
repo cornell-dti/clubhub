3bc51e005f16a5b9ae9473f368a3f1307dd639eb
Enochen <darakath1128@gmail.com>
2020-10-24 14:02:48 -0400

    fix eslint and fix/disable linter errors

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,18 +1,21 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as cors from 'cors';
+import * as firebaseKey from '../firebase-key.json';
+import clubs from './clubs';
+
+const serviceAccount = firebaseKey as admin.ServiceAccount;
+
 admin.initializeApp({
-  credential: admin.credential.cert(require("../firebase-key.json")),
-  databaseURL: "https://clubhub-dev-89ca0.firebaseio.com",
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://clubhub-dev-89ca0.firebaseio.com',
 });
 
-import * as express from "express";
-import * as cors from "cors";
-import clubs from "./clubs";
-
 const app = express();
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 app.use(express.json());
 app.use(cors({ origin: true }));
-app.use("/clubs", clubs);
+app.use('/clubs', clubs);
 
 export default functions.https.onRequest(app);
