e909bf0355aaf10e43d06c8f276633b6557d0867
Enochen <darakath1128@gmail.com>
2020-10-26 18:03:02 -0400

    change/add more endpoints

--- backend/functions/src/clubs.ts
+++ backend/functions/src/clubs.ts
@@ -1,63 +1,99 @@
 import * as express from 'express';
-import * as firestore from '@google-cloud/firestore';
+import { DocumentSnapshot, Timestamp } from '@google-cloud/firestore';
 import admin from './admin';
 
 const router = express.Router();
 const db = admin.firestore();
 
+type FirebaseClub = {
+  name: string;
+  foldedName?: string;
+  category: string;
+  due: Timestamp;
+  link: string;
+  image?: string;
+};
+
+const docToClub = (doc: DocumentSnapshot) => {
+  const data = doc.data() as FirebaseClub;
+  return { ...data, due: data.due.toDate(), id: doc.id };
+};
+
 const foldName = (name: string) => name.toLowerCase().replace(/[^0-9A-Z]+/gi, '');
 
-router
-  .route('/:category?')
-  .get(async (req, res) => {
-    try {
-      const now = new Date();
-      const query = db.collection('clubs');
-      const { category } = req.params;
-      const response = await (category ? query.where('category', '==', category) : query)
-        .where('due', '>', now)
-        .orderBy('due')
-        .orderBy('foldedName')
-        .get()
-        .then((querySnapshot) => {
-          return querySnapshot.docs.map((doc) => {
-            const data = doc.data();
-            const due = data.due.toDate();
-            return {
-              name: data.name,
-              category: data.category,
-              due,
-              link: data.link,
-              image: data.image,
-            };
-          });
-        });
-      return res.status(200).send(response);
-    } catch (e) {
-      // eslint-disable-next-line no-console
-      console.log(e);
-      return res.status(500).send(e);
-    }
-  })
-  .post(async (req, res) => {
-    try {
-      const dateDue = new Date(req.body.due);
-      const timestampDue = firestore.Timestamp.fromDate(dateDue);
-      const foldedName = foldName(req.body.name);
-      await db.collection('clubs').add({
-        name: req.body.name,
-        foldedName,
-        category: req.body.category,
-        due: timestampDue,
-        link: req.body.link,
-        image: null,
-      });
-      return res.status(200).send(`Added ${req.body.name}!`);
-    } catch (e) {
-      // eslint-disable-next-line no-console
-      console.error(e);
-      return res.status(500).send(e);
-    }
-  });
+// Get All Clubs
+router.get('/', async (req, res) => {
+  try {
+    const query = db
+      .collection('clubs')
+      .where('due', '>', Timestamp.now())
+      .orderBy('due')
+      .orderBy('foldedName');
+    const response = await query.get();
+    const clubs = response.docs.map(docToClub);
+    return res.status(200).send(clubs);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    return res.status(500).send(e.message);
+  }
+});
+
+// Get Club by Firebase ID
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const query = db.collection('clubs').doc(id);
+    const doc = await query.get();
+    if(!doc.exists) throw new Error("Club doesn't exist");
+    const club = docToClub(doc);
+    return res.status(200).send(club);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    return res.status(500).send(e.message);
+  }
+});
+
+// Get Clubs by Category
+router.get('/byCategory/:category', async (req, res) => {
+  try {
+    const { category } = req.params;
+    const query = db
+      .collection('clubs')
+      .where('category', '==', category)
+      .where('due', '>', Timestamp.now())
+      .orderBy('due')
+      .orderBy('foldedName');
+    const response = await query.get();
+    const clubs = response.docs.map(docToClub);
+    return res.status(200).send(clubs);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    return res.status(500).send(e.message);
+  }
+});
+
+// Create Club
+router.post('/', async (req, res) => {
+  try {
+    const { due, name, category, link } = req.body;
+    const dateDue = new Date(due);
+    const club: FirebaseClub = {
+      name,
+      foldedName: foldName(name),
+      category,
+      due: Timestamp.fromDate(dateDue),
+      link,
+    };
+    const doc = await db.collection('clubs').add(club);
+    return res.status(200).send(doc.id);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    return res.status(500).send(e.message);
+  }
+});
 
 export default router;
