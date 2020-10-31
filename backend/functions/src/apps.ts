c058278b46600011102df15159254bef5dc83fdf
Enoch Chen <sirenochen@gmail.com>
2020-10-31 11:04:58 -0400

    rename clubs to apps

--- backend/functions/src/apps.ts
+++ backend/functions/src/apps.ts
@@ -5,7 +5,9 @@ import admin from './admin';
 const router = express.Router();
 const db = admin.firestore();
 
-type FirebaseClub = {
+const appCollection = db.collection('clubs');
+
+type FirebaseApp = {
   name: string;
   foldedName?: string;
   category: string;
@@ -14,24 +16,23 @@ type FirebaseClub = {
   image?: string;
 };
 
-const docToClub = (doc: DocumentSnapshot) => {
-  const data = doc.data() as FirebaseClub;
+const docToApp = (doc: DocumentSnapshot) => {
+  const data = doc.data() as FirebaseApp;
   return { ...data, due: data.due.toDate(), id: doc.id };
 };
 
 const foldName = (name: string) => name.toLowerCase().replace(/[^0-9A-Z]+/gi, '');
 
-// Get All Clubs
+// Get All Apps
 router.get('/', async (req, res) => {
   try {
-    const query = db
-      .collection('clubs')
+    const query = appCollection
       .where('due', '>', Timestamp.now())
       .orderBy('due')
       .orderBy('foldedName');
     const response = await query.get();
-    const clubs = response.docs.map(docToClub);
-    return res.status(200).send(clubs);
+    const apps = response.docs.map(docToApp);
+    return res.status(200).send(apps);
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error(e);
@@ -39,15 +40,15 @@ router.get('/', async (req, res) => {
   }
 });
 
-// Get Club by Firebase ID
+// Get Apps by Firebase ID
 router.get('/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const query = db.collection('clubs').doc(id);
+    const query = appCollection.doc(id);
     const doc = await query.get();
-    if(!doc.exists) throw new Error("Club doesn't exist");
-    const club = docToClub(doc);
-    return res.status(200).send(club);
+    if(!doc.exists) throw new Error("App doesn't exist");
+    const app = docToApp(doc);
+    return res.status(200).send(app);
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error(e);
@@ -55,19 +56,18 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// Get Clubs by Category
+// Get Apps by Category
 router.get('/byCategory/:category', async (req, res) => {
   try {
     const { category } = req.params;
-    const query = db
-      .collection('clubs')
+    const query = appCollection
       .where('category', '==', category)
       .where('due', '>', Timestamp.now())
       .orderBy('due')
       .orderBy('foldedName');
     const response = await query.get();
-    const clubs = response.docs.map(docToClub);
-    return res.status(200).send(clubs);
+    const apps = response.docs.map(docToApp);
+    return res.status(200).send(apps);
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error(e);
@@ -75,19 +75,19 @@ router.get('/byCategory/:category', async (req, res) => {
   }
 });
 
-// Create Club
+// Create Apps
 router.post('/', async (req, res) => {
   try {
     const { due, name, category, link } = req.body;
     const dateDue = new Date(due);
-    const club: FirebaseClub = {
+    const app: FirebaseApp = {
       name,
       foldedName: foldName(name),
       category,
       due: Timestamp.fromDate(dateDue),
       link,
     };
-    const doc = await db.collection('clubs').add(club);
+    const doc = await appCollection.add(app);
     return res.status(200).send(doc.id);
   } catch (e) {
     // eslint-disable-next-line no-console
