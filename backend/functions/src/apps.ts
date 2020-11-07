57b545b35b28b5fafc8787269080c5c460ebf52f
unknown <loiter97@gmail.com>
2020-11-08 01:05:51 +0900

    configured backend to fit new document

--- backend/functions/src/apps.ts
+++ backend/functions/src/apps.ts
@@ -8,8 +8,8 @@ const db = admin.firestore();
 const appCollection = db.collection('apps');
 
 type FirebaseApp = {
-  name: string;
-  position: string;
+  clubName: string;
+  appName: string;
   foldedName?: string;
   category: string;
   due: Timestamp;
@@ -22,7 +22,7 @@ const docToApp = (doc: DocumentSnapshot) => {
   return { ...data, due: data.due.toDate(), id: doc.id };
 };
 
-const foldName = (name: string) => name.toLowerCase().replace(/[^0-9A-Z]+/gi, '');
+const foldName = (clubName: string) => clubName.toLowerCase().replace(/[^0-9A-Z]+/gi, '');
 
 // Get All Apps
 router.get('/', async (req, res) => {
@@ -34,22 +34,6 @@ router.get('/', async (req, res) => {
     const response = await query.get();
     const apps = response.docs.map(docToApp);
     return res.status(200).send(apps);
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e);
-    return res.status(500).send(e.message);
-  }
-});
-
-// Rename Collection
-router.get('/rename/', async (req, res) => {
-  try {
-    const docs = await appCollection.get();
-    docs.forEach((doc) => {
-      const data = doc.data();
-      db.collection('apps').doc(doc.id).set(data);
-    });
-    return res.status(200).send();
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error(e);
@@ -92,34 +76,15 @@ router.get('/byCategory/:category', async (req, res) => {
   }
 });
 
-// Get Apps by Position
-router.get('/byPosition/:position', async (req, res) => {
-  try {
-    const { position } = req.params;
-    const query = appCollection
-      .where('position', '==', position)
-      .where('due', '>', Timestamp.now())
-      .orderBy('due')
-      .orderBy('foldedName');
-    const response = await query.get();
-    const apps = response.docs.map(docToApp);
-    return res.status(200).send(apps);
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e);
-    return res.status(500).send(e.message);
-  }
-});
-
 // Create Apps
 router.post('/', async (req, res) => {
   try {
-    const { due, name, position, category, link } = req.body;
+    const { due, clubName, appName, category, link } = req.body;
     const dateDue = new Date(due);
     const app: FirebaseApp = {
-      name,
-      position,
-      foldedName: foldName(name),
+      clubName,
+      appName,
+      foldedName: foldName(clubName),
       category,
       due: Timestamp.fromDate(dateDue),
       link,
