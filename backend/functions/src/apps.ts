75db46ba9349861e9d9a9a72a06f2e2eb1efb4f9
unknown <loiter97@gmail.com>
2020-11-07 23:48:48 +0900

    changed collection name & added position field for applications

--- backend/functions/src/apps.ts
+++ backend/functions/src/apps.ts
@@ -5,10 +5,11 @@ import admin from './admin';
 const router = express.Router();
 const db = admin.firestore();
 
-const appCollection = db.collection('clubs');
+const appCollection = db.collection('apps');
 
 type FirebaseApp = {
   name: string;
+  position: string;
   foldedName?: string;
   category: string;
   due: Timestamp;
@@ -40,13 +41,29 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Rename Collection
+router.get('/rename/', async (req, res) => {
+  try {
+    const docs = await appCollection.get();
+    docs.forEach((doc) => {
+      const data = doc.data();
+      db.collection('apps').doc(doc.id).set(data);
+    });
+    return res.status(200).send();
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    return res.status(500).send(e.message);
+  }
+});
+
 // Get Apps by Firebase ID
 router.get('/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const query = appCollection.doc(id);
     const doc = await query.get();
-    if(!doc.exists) throw new Error("App doesn't exist");
+    if (!doc.exists) throw new Error("App doesn't exist");
     const app = docToApp(doc);
     return res.status(200).send(app);
   } catch (e) {
@@ -75,13 +92,33 @@ router.get('/byCategory/:category', async (req, res) => {
   }
 });
 
+// Get Apps by Position
+router.get('/byPosition/:position', async (req, res) => {
+  try {
+    const { position } = req.params;
+    const query = appCollection
+      .where('position', '==', position)
+      .where('due', '>', Timestamp.now())
+      .orderBy('due')
+      .orderBy('foldedName');
+    const response = await query.get();
+    const apps = response.docs.map(docToApp);
+    return res.status(200).send(apps);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    return res.status(500).send(e.message);
+  }
+});
+
 // Create Apps
 router.post('/', async (req, res) => {
   try {
-    const { due, name, category, link } = req.body;
+    const { due, name, position, category, link } = req.body;
     const dateDue = new Date(due);
     const app: FirebaseApp = {
       name,
+      position,
       foldedName: foldName(name),
       category,
       due: Timestamp.fromDate(dateDue),
