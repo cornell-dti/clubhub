2d66084b4b883350afe0db3fbafaa24256913776
unknown <loiter97@gmail.com>
2020-12-06 02:03:56 +0900

    added testing for root path

--- backend/functions/src/__tests__/apps.ts
+++ backend/functions/src/__tests__/apps.ts
@@ -1,12 +1,27 @@
 import * as request from 'supertest';
 import server from '../index';
 
-describe('Test ', () => {
-  it('should response the GET method', (done) => {
+type ResponseDoc = {
+  appName: string;
+  clubName: string;
+  foldedName?: string;
+  category: string;
+  due: string;
+  link: string;
+  image?: string;
+};
+
+describe('Test the root path', () => {
+  it('should check status code & date filtering', (done) => {
     request(server)
       .get('/apps')
+      .expect(200)
       .then((res) => {
-        expect(res.status).toBe(200);
+        Array.prototype.forEach.call(res.body, (doc: ResponseDoc) => {
+          const now = Date.now();
+          const appDue = Date.parse(doc.due);
+          expect(now).toBeLessThan(appDue);
+        });
         done();
       });
   });
