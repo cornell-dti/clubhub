814290ed713a66f2a0cc5cf15e65058ddf897c4b
kw433 <kw433@cornell.edu>
2020-12-07 21:38:56 -0500

    changed import and package.json

--- frontend/src/styling/StyledHome.ts
+++ frontend/src/styling/StyledHome.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { colors } from '../constants/colors';
+import { colors } from '../constants';
 import searchIcon from '../assets/search_icon.svg';
 import { device } from './Media';
 
