78fc96fc936c3174222064234e78365773fae961
Enochen <darakath1128@gmail.com>
2021-02-13 14:53:50 -0500

    add hotjar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Admin from './components/Admin';
 import Home from './components/Home';
+import { hotjar } from 'react-hotjar';
+import { HJID, HJSV } from './constants/hotjar';
+
+hotjar.initialize(HJID, HJSV);
 
 const App = () => {
   return (
