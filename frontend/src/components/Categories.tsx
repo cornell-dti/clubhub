c71ac953571aa0da7f9f6330a42d9eb2b20b0462
Enochen <darakath1128@gmail.com>
2021-02-13 15:39:42 -0500

    switch to new categories

--- frontend/src/components/Categories.tsx
+++ frontend/src/components/Categories.tsx
@@ -7,15 +7,12 @@ const categories = () => {
       <h2>Categories</h2>
       <ul>
         <li><a href="#all">All</a></li>
-        <li><a href="#academic">Academic</a></li>
-        <li><a href="#cultural">Cultural</a></li>
-        <li><a href="#games">Games</a></li>
-        <li><a href="#greek">Greek</a></li>
-        <li><a href="#political">Political</a></li>
-        <li><a href="#project-team">Project Team</a></li>
-        <li><a href="#publication-media">Publication/Media</a></li>
-        <li><a href="#sustainability">Sustainability</a></li>
-        <li><a href="#sports">Sports</a></li>
+        <li><a href="#impact">Impact</a></li>
+        <li><a href="#social">Social</a></li>
+        <li><a href="#entrepreneurship">Entrepreneurship</a></li>
+        <li><a href="#consulting">Consulting</a></li>
+        <li><a href="#education">Education</a></li>
+        <li><a href="#opportunity">Opportunity</a></li>
       </ul>
     </CategoriesContainer>
   );
