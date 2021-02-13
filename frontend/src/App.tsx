d377a40160ed082868690dc8879d320b9a21179e
Enochen <darakath1128@gmail.com>
2021-02-13 16:06:31 -0500

    put hotjar constants in index.ts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Admin from './components/Admin';
 import Home from './components/Home';
 import { hotjar } from 'react-hotjar';
-import { HJID, HJSV } from './constants/hotjar';
+import { HJID, HJSV } from './constants';
 
 hotjar.initialize(HJID, HJSV);
 
