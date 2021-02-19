263be33318649fa06bdb4412af50620a5b75dc34
kw433 <kw433@cornell.edu>
2021-02-18 22:31:23 -0500

    Resolve merge conflict

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { HeaderContainer } from '../styling/StyledHome';
 
-const header = ({ searchHandler }: Props) => {
+const header = () => {
   return (
     <HeaderContainer>
       <img src={require("../assets/clubhub_logo.svg")} alt="logo" />
-      <input type="text" onChange={searchHandler} placeholder="Search..." />
     </HeaderContainer>
   );
 };
 
 export default header;
-
-interface Props {
-  searchHandler(event: React.ChangeEvent<HTMLInputElement>): void
-}
